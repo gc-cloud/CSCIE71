069613d61de1a012973563682553e958e40ee75e
Gerardo Castaneda <gerardocastaneda@g.harvard.edu>
2016-09-01 22:01:07 -0500

    new assignment 1 - hello

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.example.helloworld;
+
+/**
+ * Created by gerardocastaneda on 8/30/16.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, " +args[0]+"!");
+    }
+
+
+}
